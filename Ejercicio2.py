a1b947e11867517bfbe33dce45d439159595830b
Francesco <stjs300@gmail.com>
2019-11-20 08:29:26 -0500

    asdas

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,10 +1,18 @@
 letra = 0
 
-a = input("Coloque texto:")
+a = input("Coloque texto: ")
+b = input("Coloque letra a copiar: ")
 
+def contarletra(a, b):
+    contador = 0
+    for algo in a.lower():
+        if algo == b.lower():
+            contador = contador + 1
+    return contador
 
-def contarletra(texto, letra):
-    
+contadorMas = contarletra(a,b)
+print(contadorMas)
+
     
     
     
